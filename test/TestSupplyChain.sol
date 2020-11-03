85bae233cb71e090e66352699df565f2be46ed16
SHAILENDRA OMKAR <shailendrakumaromkar@gmail.com>
2020-11-03 18:31:02 +0530

    Update TestSupplyChain.sol

--- test/TestSupplyChain.sol
+++ test/TestSupplyChain.sol
@@ -6,34 +6,6 @@ import "../contracts/SupplyChain.sol";
 
 contract TestSupplyChain {
 
- /*SupplyChain supplychainInstance = SupplyChain(DeployedAddresses.SupplyChain());
-    address acc0;
-    address acc1;
-    address acc2;
-
-     function beforeAll() public {
-        acc0 = supplychainInstance.getAccount(0); 
-        acc1 = supplychainInstance.getAccount(1);
-        acc2 = supplychainInstance.getAccount(2);
-     }
-    
-    function testOwnerNotWorking() public returns (bool) {
-        return Assert.equal(supplychainInstance.owner(),acc0,"owner is different");
-        
-    }
-	*/
-
-    function testOwnerWorking() public returns (bool) {
-           SupplyChain supplychainInstance = SupplyChain(DeployedAddresses.SupplyChain());
-         return Assert.equal(supplychainInstance.owner(),msg.sender,"owner is different");
-        
-     }
-
-    
-    // function beforeEach() public{
-    //     supplychainInstance = new SupplyChain();
-        
-    // }
 
     // Test for failing conditions in this contracts:
     // https://truffleframework.com/tutorials/testing-for-throws-in-solidity-tests
